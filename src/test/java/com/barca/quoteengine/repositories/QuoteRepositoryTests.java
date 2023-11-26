ce616853334f732f57d95470671c7bc96c437561
Gianmarco Barca <Gianmarco.technology@outlook.com>
2023-11-26 00:46:54 -0500

    quote creation validation test implemented for the repository layer

--- src/test/java/com/barca/quoteengine/repositories/QuoteRepositoryTests.java
+++ src/test/java/com/barca/quoteengine/repositories/QuoteRepositoryTests.java
@@ -1,10 +1,12 @@
 package com.barca.quoteengine.repositories;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.barca.quoteengine.configs.MongoConfig;
 import com.barca.quoteengine.dtos.QuoteResponseDto;
 import com.barca.quoteengine.model.Quote;
+
+import jakarta.validation.ConstraintViolationException;
 
 @DataMongoTest
 @Transactional
@@ -50,5 +54,16 @@ class QuoteRepositoryTests {
     // ASSERT
     assertThat(quote).hasSize(sampleSize);
 
+  }
+
+  @Test
+  void save_quote_should_fail_validation() {
+
+    // ARRANGE
+    Quote quote = new Quote(null, null, null);
+
+    // ACT AND ASSERT
+    assertThrowsExactly(ConstraintViolationException.class,
+        () -> quoteRepository.save(quote));
   }
 }
